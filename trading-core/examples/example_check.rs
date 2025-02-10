e3f171ed823662f203a94640c64386b466e2df59
Erio-Harrison <u7541840@gmail.com>
2025-02-10 22:31:21 +1100

    Fix issue #1: Resolve problem in example_check.rs

--- trading-core/examples/example_check.rs
+++ trading-core/examples/example_check.rs
@@ -7,23 +7,24 @@ pub mod polkadot {}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Create an API client for Polkadot.
+    // Create a new API client, configured to talk to Polkadot nodes.
     let api = OnlineClient::<PolkadotConfig>::new().await?;
 
     // Build a balance transfer extrinsic.
-    let dest = dev::bob().public_key().into(); // ERROR OCCURS HERE
+    let dest = dev::bob().public_key().into();
     let balance_transfer_tx = polkadot::tx().balances().transfer_allow_death(dest, 10_000);
 
-    // Submit the extrinsic from Alice and wait for it to be finalized.
+    // Submit the balance transfer extrinsic from Alice, and wait for it to be successful
+    // and in a finalized block. We get back the extrinsic events if all is well.
     let from = dev::alice();
     let events = api
         .tx()
-        .sign_and_submit_then_watch_default(&balance_transfer_tx, &from) // ERROR OCCURS HERE
+        .sign_and_submit_then_watch_default(&balance_transfer_tx, &from)
         .await?
         .wait_for_finalized_success()
         .await?;
 
-    // Find and print the Transfer event.
+    // Find a Transfer event and print it.
     let transfer_event = events.find_first::<polkadot::balances::events::Transfer>()?;
     if let Some(event) = transfer_event {
         println!("Balance transfer success: {event:?}");
